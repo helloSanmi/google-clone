0b5fceef0bd6d692e8e8db8ec5336f70044d7550
helloSanmi <idowuoluwasanmi@yahoo.com>
2022-07-21 22:42:04 +0100

    update

--- src/App.js
+++ src/App.js
@@ -1,25 +1,27 @@
-import logo from './logo.svg';
-import './App.css';
+import React, { useState } from 'react'
+import { NavBar } from './components/NavBar';
+import { Footer } from './components/Footer';
+import { Routes } from './components/Routes';
 
-function App() {
+const App = () => {
+
+    const [darkTheme, setDarkTheme] = useState(false)
+
+    const toggleButton = () => darkTheme ? setDarkTheme(false) : setDarkTheme(true)
+
+
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+    <div className={ darkTheme ? 'dark' : ''}>
+        <div className="bg-gray-100 dark:bg-gray-900 dark:text-gray-50 min-h-screen">
+            <NavBar />
+            <Routes />
+            <Footer />
+            <button className="py-2 px-4 bg-blue-500 text-white font-semibold rounded-lg shadow-md hover:bg-blue-700 focus:outline-none focus:ring-2 focus:ring-blue-400 focus:ring-opacity-75"
+             onClick={toggleButton}>Toggle</button>
+        </div>      
     </div>
-  );
+  )
 }
+
 
 export default App;
