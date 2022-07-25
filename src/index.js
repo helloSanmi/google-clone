aa84b0e9f48a50e4796280ee0c505a759db649b7
helloSanmi <idowuoluwasanmi@yahoo.com>
2022-07-25 21:52:29 +0100

    update

--- src/index.js
+++ src/index.js
@@ -1,14 +1,15 @@
 import React from 'react';
 import ReactDOM from 'react-dom/client';
+import { BrowserRouter as Router } from 'react-router-dom'
 import './css/global.css';
 import App from './App';
 
 
 const root = ReactDOM.createRoot(document.getElementById('root'));
 root.render(
-  <React.StrictMode>
-    <App />
-  </React.StrictMode>
+    <Router>
+      <App />
+    </Router>
 );
 
 // ReactDOM.render(<App />, document.getElementById('root'));
